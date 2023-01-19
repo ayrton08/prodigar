73447e5fd401501b03b04fd33c0c82023f5bac5e
Leandro Roldan <leandrom.roldan@gmail.com>
2023-01-19 20:37:01 -0300

    add: ui/typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+		'./ui/**/*.{js,ts,jsx,tsx}',
+		'./src/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				montserrat: ['Montserrat Alternates', 'sans-serif'],
+				poppins: ['Poppins', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+}

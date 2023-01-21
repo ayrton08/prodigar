b4b565927d8c4ac3197fe038c5fb9e0ce41a992a
Maxi Jofré <maxijofre.c@gmail.com>
2023-01-21 11:59:43 -0300

    adding the map preview

---
+++ components/MapPreview.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import map from "../public/map.svg";
+import { Title, Large } from "../ui/typography";
+
+export const MapPreview = () => {
+  return (
+    <section className=" w-full p-4 flex flex-col items-center md:flex-row-reverse text-[#2C3E50] justify-evenly bg-[#FEF5E7]">
+      <div className="card__map">
+        <Image src={map} alt="" className="img zoom" />
+      </div>
+      <div className="max-w-[600px] lg:w-[800px] md:w-1/2 md:px-20 py-8 flex flex-col justify-between gap-10">
+        <Title>Da a conocer la ubicación de tus pertenencias</Title>
+        <Large>
+          En esta sección podrás publicar en el mapa la ubicación de tus
+          pertenencias para que sepan donde retirarlas, o buscar donde están
+          ubicadas las pertenencias de otras personas.
+        </Large>
+      </div>
+    </section>
+  );
+};

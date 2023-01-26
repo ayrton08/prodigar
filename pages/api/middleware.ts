7258583860d7dec94205f5a0caa125c3319d4859
Ayrton <ayrtonjuarez90@gmail.com>
2023-01-26 12:14:33 -0300

    fix: cors issue

---
+++ pages/api/middleware.ts
@@ -0,0 +1,19 @@
+import type { NextRequest } from "next/server";
+
+export function middleware(req: any) {
+  if (req.method == "OPTIONS") {
+    return new Response("", {
+      status: 204,
+      headers: {
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+        "Access-Control-Allow-Headers": req.headers.get(
+          "Access-Control-Request-Headers"
+        ),
+        Vary: "Access-Control-Request-Headers",
+        "Content-Length": "0",
+      },
+    });
+  }
+}

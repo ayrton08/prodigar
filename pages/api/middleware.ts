562cb3f57d2ab18518dced687bc94a4a0eaf9af3
Ayrton <ayrtonjuarez90@gmail.com>
2023-01-26 12:19:12 -0300

    fix: middleware types

--- pages/api/middleware.ts
+++ pages/api/middleware.ts
@@ -1,6 +1,6 @@
 import type { NextRequest } from "next/server";
 
-export function middleware(req: any) {
+export function middleware(req: NextRequest) {
   if (req.method == "OPTIONS") {
     return new Response("", {
       status: 204,
@@ -8,9 +8,6 @@ export function middleware(req: any) {
         "Access-Control-Allow-Credentials": "true",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-        "Access-Control-Allow-Headers": req.headers.get(
-          "Access-Control-Request-Headers"
-        ),
         Vary: "Access-Control-Request-Headers",
         "Content-Length": "0",
       },

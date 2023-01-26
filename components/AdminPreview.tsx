b960cb2bda879cf221d72cdb24b215c98e188e90
Ayrton <ayrtonjuarez90@gmail.com>
2023-01-26 10:56:30 -0300

    feat: text infomation for card's

--- components/AdminPreview.tsx
+++ components/AdminPreview.tsx
@@ -5,7 +5,7 @@ import { Information } from "./Information";
 export const AdminPreview = () => {
   return (
     <section className=" w-full p-4 flex flex-col items-center md:flex-row-reverse text-[#2C3E50] justify-evenly bg-[#FEF5E7]" id="admin">
-      <div className="max-w-[600px] lg:w-[800px] md:w-1/2 md:px-20 py-8 flex flex-col justify-between gap-10">
+      <div className="max-w-[600px] lg:w-[800px] md:w-1/2 md:px-10 py-8 flex flex-col justify-between gap-10">
         <Title>Administra tus posteos realizados</Title>
         <div className=" p-3 rounded-md bg-slate-500 bg-opacity-10 shadow-md">
           <Information title="Informacion Personal">
--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -4,27 +4,26 @@ import { ReportForm } from "./ReportForm";
 
 export const PostPreview = () => {
   return (
-    <section className=" w-full p-4 flex flex-col items-center md:flex-row text-[#2C3E50] justify-evenly bg-[#FEF5E7]" id="post">
-      <div className="max-w-[600px] lg:w-[800px] md:w-1/2 md:px-20 py-8 flex flex-col justify-between gap-10">
-        <Title>Publica objetos o pertenencias que ya no usas</Title>
+    <section
+      className=" w-full p-4 flex flex-col items-center md:flex-row text-[#2C3E50] justify-evenly bg-[#FEF5E7]"
+      id="post"
+    >
+      <div className="max-w-[600px] lg:w-[800px] md:w-1/2 md:px-10 py-8 flex flex-col justify-between gap-10">
+        <Title>Publica pertenencias que ya no usas</Title>
         <div className=" p-3 rounded-md bg-slate-500 bg-opacity-10 shadow-md">
-          <Information title="Informacion Personal">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Repellendus minus quod quam aperiam cumque illo deserunt rerum
-            facere dolore tenetur magni, tempora ut iusto quis mollitia
-            perferendis rem vel alias.
+          <Information title="Informacion">
+            Podrás compartir información sobre lo que te gustaria donar como el
+            nombre del objeto. También podras incluir una descripción, donde
+            puedas brindar más infomacion, por ejemplo si es necesario algun
+            medio de transporte para retirarlo.
           </Information>
           <Information title="Ubicacion">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Repellendus minus quod quam aperiam cumque illo deserunt rerum
-            facere dolore tenetur magni, tempora ut iusto quis mollitia
-            perferendis rem vel alias.
+            Con un sistema de geolocalización podrás publicar tu ubicación
+            exacta donde te gustaria que retiran la donación.
           </Information>
           <Information title="Imagen">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Repellendus minus quod quam aperiam cumque illo deserunt rerum
-            facere dolore tenetur magni, tempora ut iusto quis mollitia
-            perferendis rem vel alias.
+            Un imagen dice más que muchas palabras, por lo que también podrás
+            publicar una imagen del objeto.
           </Information>
         </div>
       </div>
